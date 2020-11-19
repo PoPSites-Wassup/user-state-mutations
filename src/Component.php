609215c0e8b8431744fee06f4d298ce75929f597
Leonardo Losoviz <leo@getpop.org>
2020-11-19 21:45:04 +0800

    Moved to PoPSchema\UserStateMutations

--- src/Component.php
+++ src/Component.php
@@ -19,7 +19,7 @@ class Component extends AbstractComponent
     public static function getDependedComponentClasses(): array
     {
         return [
-            \PoPSchema\UserState\Component::class,
+            \PoPSchema\UserStateMutations\Component::class,
         ];
     }
 }
--- src/MutationResolvers/MutationInputProperties.php
+++ src/MutationResolvers/MutationInputProperties.php
@@ -6,8 +6,6 @@ namespace PoPSitesWassup\UserStateMutations\MutationResolvers;
 
 class MutationInputProperties
 {
-    public const USERNAME_OR_EMAIL = 'usernameOrEmail';
-    public const PASSWORD = 'password';
     public const USER_LOGIN = 'userLogin';
     public const CODE = 'code';
     public const REPEAT_PASSWORD = 'repeatPassword';
